32fb06cf871a908d242d0da2407fb4ca91e1d621
yazzy4 <yazzy4@gmail.com>
2019-01-24 17:37:23 -0500

    Added Files

---
+++ ProgrammaticTextProject/DetailView.swift
@@ -0,0 +1,21 @@
+//
+//  DetailView.swift
+//  ProgrammaticTextProject
+//
+//  Created by Yaz Burrell on 1/24/19.
+//  Copyright © 2019 Yaz Burrell. All rights reserved.
+//
+
+import UIKit
+
+class DetailView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ ProgrammaticTextProject/MainView.swift
@@ -0,0 +1,21 @@
+//
+//  MainView.swift
+//  ProgrammaticTextProject
+//
+//  Created by Yaz Burrell on 1/24/19.
+//  Copyright © 2019 Yaz Burrell. All rights reserved.
+//
+
+import UIKit
+
+class MainView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
